d013058c2a5e58c9d540684bd5ccc559997f01a9
Mr-Azeez <114315712+Mr-Azeez@users.noreply.github.com>
2022-12-21 14:50:55 +0100

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+module.exports = {
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:import/recommended",
+      "plugin:jsx-a11y/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "eslint-config-prettier",
+    ],
+    settings: {
+      react: {
+        version: "detect",
+      },
+  
+      "import/resolver": {
+        node: {
+          paths: ["src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+        env: {
+          node: true,
+        },
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      // allow jsx syntax in js files (for next.js project)
+      "react/jsx-filename-extension": [
+        1,
+        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      ], //should add ".ts" if typescript project
+    },
+  };
